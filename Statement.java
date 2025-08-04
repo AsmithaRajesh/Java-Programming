51ca73dee9b9fc733669bafa5a461a7808bba34c
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-04 12:27:27 +0530

    Create Statement.java

---
+++ Statement.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int x=18;
+        int y=20;
+        if (x<y){
+        System.out.println("ELDER");
+        }
+        else{
+        System.out.println("YOUNGER");
+        }
+    }
+}
