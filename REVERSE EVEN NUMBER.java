565df167e28be15f72f2078c557b763fd8d059c8
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-05 22:55:23 +0530

    REVERSE EVEN NUMBER

---
+++ REVERSE EVEN NUMBER.java
@@ -0,0 +1,23 @@
+DEFINE:(WORKING FLOW)
+ 1. The initial condition statement (the first portion in parentheses) is executed.
+2. The boolean expression (the second portion in parentheses) is tested to see if it is
+true. If so, the body of the loop is executed, if it is false, the for loop is exited.
+3. The iteration statement (the third portion in the parentheses) is executed. It
+returns to Step 2 and continues until the loop is exited.
+SYNTAX:
+for (initial_condition; boolean_expression; iteration)
+{
+ // loop block
+}
+EXAMPLE:
+import java.util.Scanner;
+public class count{
+    public static void main(String[] args) {
+        Scanner data = new Scanner(System.in);
+        for (int i = 10; i >= 0; i--) {
+            if (i % 2 == 0) { 
+                System.out.println(i);
+            }
+        }
+    }
+}    
