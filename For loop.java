fc21bf9afd946c75f0c4955217a53d15c224c959
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-05 11:42:17 +0530

    Create For loop.java

---
+++ For loop.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        Scanner data = new Scanner(System.in);
+        
+        for(int count=1;count<=10;count++)
+        {
+            System.out.println(count);
+        }
+        
+    } 
+}
+SYNTAX:
+for (initial_condition; boolean_expression; iteration)
+{
+ // loop block
+}
