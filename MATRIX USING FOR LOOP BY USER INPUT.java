763c5fe8ebc0dd764aa17c956cdab95253af336c
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-05 23:08:59 +0530

    MATRIX USING FOR LOOP BY USER INPUT

--- MATRIX USING FOR LOOP BY USER INPUT.java
+++ MATRIX USING FOR LOOP BY USER INPUT.java
@@ -1,5 +1,17 @@
+DEFINE:(WORKING FCLOW)
+The following steps describe how the for loop works.
+1. The initial condition statement (the first portion in parentheses) is executed.
+2. The boolean expression (the second portion in parentheses) is tested to see if it is
+true. If so, the body of the loop is executed, if it is false, the for loop is exited.
+3. The iteration statement (the third portion in the parentheses) is executed. It
+returns to Step 2 and continues until the loop is exited.
+SYNTAX:
+for (initial_condition; boolean_expression; iteration)
+{
+ // loop block
+}    
+EXAMPLE:
 import java.util.Scanner;
-
 public class NestedLoop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
