dd3012a5a316424ebbcfeedbfd6877beb34d82c7
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-05 22:38:53 +0530

    For loop

--- For loop.java
+++ For loop.java
@@ -1,5 +1,16 @@
+DEFINE:(WORKING FLOW)
+1. The initial condition statement (the first portion in parentheses) is executed.
+2. The boolean expression (the second portion in parentheses) is tested to see if it is
+true. If so, the body of the loop is executed, if it is false, the for loop is exited.
+3. The iteration statement (the third portion in the parentheses) is executed. It
+returns to Step 2 and continues until the loop is exited.
+SYNTAX:
+for (initial_condition; boolean_expression; iteration)
+{
+ // loop block
+}
+EXAMPLE:
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         int num;
@@ -11,9 +22,4 @@ public class Main {
         }
         
     } 
-}
-SYNTAX:
-for (initial_condition; boolean_expression; iteration)
-{
- // loop block
 }
