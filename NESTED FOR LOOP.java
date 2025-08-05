f2af47f881db2d3b51a2d1b2ffccf3bf5e9dbe1c
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-05 22:51:49 +0530

    NESTED FOR LOOP

--- NESTED FOR LOOP.java
+++ NESTED FOR LOOP.java
@@ -1,3 +1,11 @@
+DEFINE:
+FOR LOOP INSIDE A FOR LOOP IS NESTED FOR
+SYNTAX:
+for (initial_condition; boolean_expression; iteration)
+{
+ // loop block
+}
+EXAMPLE:
 public class main {
     public static void main(String[] args) {
         for (int i = 1; i <= 2; i++) {
