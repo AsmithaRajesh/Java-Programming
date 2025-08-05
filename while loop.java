73cf85cdf2ff1edc24ed11c46675ad708c716715
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-05 11:26:46 +0530

    Create while loop.java

---
+++ while loop.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        Scanner data = new Scanner(System.in);
+        System.out.println("enter the number");
+        num = data.nextInt();
+        while (num >= 50) ;
+        {
+            if (num % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        }
+        
+        
+    } 
+}
+SYNTAX:
+while (boolean_expression)
+{
+ // loop block
+}
