32b305223edd63f74f3cbe2c2dd2ec9e0c7bb453
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-06 09:35:46 +0530

    CREATION OF ARRAY

---
+++ CREATION OF ARRAY.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class main{
+    public static void main(String[]args){
+        String[] array={"ASMI","DEVI","PRAVII","MAHA","LALLI","ARCH"
+            
+        };
+        array[1]="ASMI";
+        System.out.println(array[2]);
+    }
+}
