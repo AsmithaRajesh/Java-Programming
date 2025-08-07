0a5dbd574d46343210805de8b0739c3dfc0138cd
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-07 09:39:01 +0530

    PATTERN.java

--- PATTERN.java
+++ PATTERN.java
@@ -1,3 +1,4 @@
+NUMBER PATTERN:
 import java.util.Scanner ;
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +9,20 @@ public class Main {
                 System.out.print( j + " ");
             }
             System.out.println(); 
+        }
+    }
+}
+
+REVERSE PATTERN:
+public class Main {
+    public static void main(String[] args) {
+        int n = 8;
+
+        for (int i = n; i >= 1; i--) {  
+            for (int j = 1; j <= i; j++) { 
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
     }
 }
