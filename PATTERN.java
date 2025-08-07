9b4a36718c06751311d4d6e9f7c28ad0f62507c4
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-07 10:18:21 +0530

    PATTERN

--- PATTERN.java
+++ PATTERN.java
@@ -70,3 +70,23 @@ public class Main {
         }
     }
 }
+
+#PYRAMID PATTERN:
+public class Main {
+    public static void main(String[] args) {
+        int n = 4;  
+
+        for (int i = 1; i <= n; i++) {
+            
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
