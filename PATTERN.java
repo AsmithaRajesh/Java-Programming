3d4b3237a849990606f34d57e082817aee77b515
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-07 10:00:40 +0530

    PATTERN.java

--- PATTERN.java
+++ PATTERN.java
@@ -26,3 +26,47 @@ public class Main {
         }
     }
 }
+
+CONTINUOUS RANGE NUMBER PATTERN:
+public class Main {
+    public static void main(String[] args) {
+        int num = 1;      
+        int n = 6;
+        for (int i = 1; i <= n; i++) {    
+            for (int j = 1; j <= i; j++) { 
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
+SQUARE VALUES PATTERN:
+public class Main {
+    public static void main(String[] args) {
+        int num = 1;      
+        int n = 6;
+        for (int i = 1; i <= n; i++) {        
+            for (int j = 1; j <= i; j++) {    
+                System.out.print((num * num) + "   ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+REMAINSER VALUES PATTERN:
+public class Main {
+    public static void main(String[] args) {
+        int num = 1;
+        int n = 6;
+        for (int i = 1; i <= n; i++) {        
+            for (int j = 1; j <= i; j++) {    
+                System.out.print(((num * num) % 2) + " ");  
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
