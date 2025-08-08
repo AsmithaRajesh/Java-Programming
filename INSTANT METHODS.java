faff7a209a066672feee503a25f5f770ada19b65
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-08 14:02:14 +0530

    INSTANT METHODS

---
+++ INSTANT METHODS.java
@@ -0,0 +1,17 @@
+SYNTAX:
+modifier returntyp name (){
+  //method body
+}
+
+#EXAMPLE: 
+class Main {
+    public static void main(String[] args) {
+        System.out.println(" ");
+        Main obj = new Main(); 
+        obj.sample();        
+    }
+    public void sample() {    
+        int a = 10;
+        System.out.println(a); 
+    }
+}
