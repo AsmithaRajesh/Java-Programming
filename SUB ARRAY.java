7ac8403297b5b0fb572912db238db60c6e994f0e
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-11 15:42:27 +0530

    SUB ARRAY

--- SUB ARRAY.java
+++ SUB ARRAY.java
@@ -12,3 +12,21 @@ class Main {
         }
         }
 }
+
+#MERGING OF SUB ARRAY:
+class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3,4};
+        int[] arr2 = { 5, 6,7,8};
+        int[] merged = new int[arr1.length + arr2.length];
+        for (int i = 0; i < arr1.length; i++) {
+            merged[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            merged[arr1.length + i] = arr2[i];
+        }
+        for (int num : merged) {
+            System.out.print(num + " " );
+        }
+    }
+}
