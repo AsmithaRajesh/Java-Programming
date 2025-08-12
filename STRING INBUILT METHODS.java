0b17ac6b001570fc1d3974b49a7dfb468a0eb430
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2025-08-12 10:16:21 +0530

    STRING INBUILT METHODS

--- STRING INBUILT METHODS.java
+++ STRING INBUILT METHODS.java
@@ -3,20 +3,29 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner data = new Scanner(System.in);
-        System.out.println("Enter your Name:");
+        System.out.println("Enter Name1:");
         String str = data.nextLine();
-        #charAt()
+        System.out.println("Enter Name2:");
+        String str1=data.nextLine();
+        
         char ch = str.charAt(0);
         System.out.println("The Character is: " + ch);
-        #codePointBefore()
+       
         char ch1 = (char) str.codePointBefore(3);
         System.out.println("The Character Before is: " + ch1);
-        #codePointAt()
+      
         char ch2 = (char) str.codePointAt(4);
         System.out.println("The Character After is: " + ch2);
-        #codePointCount()
+      
         int ch3 = str.codePointCount(0, 4);
         System.out.println("The number is: " + ch3);
+        
+        int ch4=str.compareTo(str1);
+        System.out.println("Result:"+ch4);
+        
+       
+        
+        
 
     
     }
