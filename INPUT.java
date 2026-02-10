3312b6dece67e487cd183592275a55b8e66550a0
AsmithaRajesh <asmitha9180@mountzion.ac.in>
2026-02-10 10:09:54 +0530

    Create INPUT.java

---
+++ INPUT.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main
+{
+    public static void main (String[]args) {
+        Scanner sc = new Scanner(System.in);
+        byte a = sc.nextByte();
+        short b = sc.nextShort();
+        int c = sc.nextInt();
+        float d = sc.nextFloat();
+        long e = sc.nextLong();
+        double f = sc.nextDouble();
+        String str1 = sc.next();
+        sc.nextLine();//getting string input between some values
+        String str2 = sc.nextLine();
+        char ch = sc.next .charAt(3);
+        System.out.println(a+" "+b);
+        System.out.println(c+" "+d);
+        System.out.println(e+" "+f);
+        System.out.println(str1+" "+str2);
+        System.out.println(ch);
+        
+        
+    }
+}
+
+OUTPUT:
+10 20
+30 40.5
+200 300.0
+Asmitha Rajesh
+I
